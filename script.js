aaa6c1b9a32d79c9969f75996f0d756ad61d125b
MikeyLpx <MichaelLpz85301@gmail.com>
2023-07-06 23:01:29 -0700

    finished adding javascript and readme

--- script.js
+++ script.js
@@ -3,15 +3,34 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
 var characterLengthText= prompt("how many charcters would you like your password to contain");
-var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var specialTxt= confirm("Click OK to confirm including special characters")
+var numbers= confirm("Click OK to confirm including numeric characters")
+var lower= confirm("Click OK to confirm including lowercase characters")
+var upper= confirm("click OK to confirm including uppercase characters")
+var specialPassword= ""
+var specialText = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var lowerTxt= "abcdefghijklmnopqrstuvwxyz"
+var UpperTxt= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numericTxt= "0123456789"
 var length =128;
 var myGeneratedPassword = '';
-if (characterLengthText != null) {
-    for(var i=0, n=characters.length; i < length; ++i) {myGeneratedPassword +=characters.charAt((Math.random()*n));
-    }
-}
 
- 
+if (specialTxt){
+  specialPassword = specialPassword + specialText
+}
+if ( numbers){
+  specialPassword= specialPassword + numericTxt
+}
+if (lower){
+  specialPassword = specialPassword + lowerTxt
+}
+if (upper){
+  specialPassword = specialPassword + UpperTxt
+}
+if (characterLengthText != null){
+  for(var i=0, n=specialPassword.length; i < characterLengthText; ++i) {myGeneratedPassword += specialPassword.charAt((Math.random()*n));
+  }
+};
 
 return myGeneratedPassword;
 }
